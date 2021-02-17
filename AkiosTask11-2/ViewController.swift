71337d9417e6cedca8aef117b97eb5119ddd3223
猫吉 <freeaddress834@gmail.com>
2021-02-18 08:15:43 +0900

    create .gitignore, add filenames in that

---
+++ AkiosTask11-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AkiosTask11-2
+//
+//  Created by Nekokichi on 2021/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
