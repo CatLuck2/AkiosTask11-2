80999878075c4dcde62d6e2fa32a361df21f2dda
猫吉 <freeaddress834@gmail.com>
2021-02-19 08:16:35 +0900

    add: TableVCでキャンセルボタン処理を記述

--- AkiosTask11-2/TableViewController/TableViewController.swift
+++ AkiosTask11-2/TableViewController/TableViewController.swift
@@ -27,4 +27,8 @@ class TableViewController: UITableViewController {
         return cell
     }
 
+    @IBAction func cancelButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+
 }
