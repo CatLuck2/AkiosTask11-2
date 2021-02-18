ab1fa1b12d472cb0d5eecadbc5ce1ecad9a769fb
猫吉 <freeaddress834@gmail.com>
2021-02-18 09:15:31 +0900

    delete: ViewController.swift、create: DisplayViewController files

---
+++ AkiosTask11-2/DisplayViewController/DisplayView.swift
@@ -0,0 +1,20 @@
+//
+//  DisplayView.swift
+//  AkiosTask11-2
+//
+//  Created by Nekokichi on 2021/02/18.
+//
+
+import UIKit
+
+class DisplayView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
