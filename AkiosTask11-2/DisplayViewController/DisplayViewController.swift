2c2192309f71e1eae8c2e722107d75ba30112fe2
猫吉 <freeaddress834@gmail.com>
2021-02-19 07:47:24 +0900

    fix: DisplayViewをDIsplayVCに差し替え、change: 初期画面をDisplayVC.xibに指定

---
+++ AkiosTask11-2/DisplayViewController/DisplayViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DisplayViewController.swift
+//  AkiosTask11-2
+//
+//  Created by Nekokichi on 2021/02/18.
+//
+
+import UIKit
+
+class DisplayViewController: UIViewController {
+
+    @IBOutlet weak var prefectureNameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func viewDidLayoutSubviews() {
+        print(prefectureNameLabel.frame)
+    }
+    
+    @IBAction func presentTableVIewController(_ sender: UIButton) {
+    }
+    
+}
--- AkiosTask11-2/SceneDelegate.swift
+++ AkiosTask11-2/SceneDelegate.swift
@@ -17,6 +17,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene as! UIWindowScene)
+        self.window = window
+        window.makeKeyAndVisible()
+        let displayVC = DisplayViewController()
+        window.rootViewController = displayVC
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
