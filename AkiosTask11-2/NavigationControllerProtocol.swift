6528a08e3ff8415419bf39bde3058a0901f90092
猫吉 <freeaddress834@gmail.com>
2021-02-19 13:12:31 +0900

    add: NavigationController用のプロトコルを宣言

---
+++ AkiosTask11-2/NavigationControllerProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  NavigationControllerProtcol.swift
+//  AkiosTask11-2
+//
+//  Created by Nekokichi on 2021/02/19.
+//
+
+import Foundation
+
+protocol NavigationControllerProtocol {
+    func setUpNavigationController()
+}
